f67affc7fb083ae3f5ae51ac17d50409cd0c4383
Alexander Luligo <alexrkm11@gmail.com>
2024-10-03 02:05:12 -0600

    Operaciones Aritmeticas

---
+++ pythonProject/1.py
@@ -0,0 +1,12 @@
+
+
+numero1 = int(input("Ingrese el primer numero: "))
+numero2 = int(input("Ingrese el segundo numero: "))
+suma = numero1 + numero2
+
+
+resta = numero1 - numero2
+division = numero1 / numero2
+multiplicacion = numero1 + numero2
+
+print("El resultado es: ","suma es:",suma,"resta es:",resta,"Division es:",division,"Multiplicacion es.", multiplicacion)
